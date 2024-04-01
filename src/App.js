5f4ac0af6bd5c447cfdef972a43cc2a06422da20
togay-aytemiz <togayaytemiz@gmail.com>
2024-04-01 19:09:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+
+export default function VideoPlayer() {
+  const ref = useRef();
+
+  console.log(ref.current);
+
+  // const togglePlayback = () => {
+  //   if (videoRef.current.paused) {
+  //     videoRef.current.play();
+  //   } else {
+  //     videoRef.current.pause();
+  //   }
+  // };
+
+  const [videoStatus, setVideoStatus] = useState(false);
+
+  const togglePlayback = (e) => {
+    let name = e.target.innerHTML;
+    if (name === "Oynat") {
+      ref.current.play();
+      setVideoStatus(true);
+    } else if (name === "Duraklat") {
+      ref.current.pause();
+      setVideoStatus(false);
+    }
+  };
+
+  return (
+    <div className="max-w-[500px] space-y-4 p-8 mx-auto">
+      <h1 className="text-center font-bold text-3xl">Video Oynatıcı</h1>
+      <video
+        ref={ref}
+        src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+        poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
+      />
+      <div className="flex justify-between">
+        {videoStatus && (
+          <button
+            className="text-orange-500 font-semibold"
+            onClick={togglePlayback}
+          >
+            Duraklat
+          </button>
+        )}
+
+        {!videoStatus && <div>Duraklatıldı</div>}
+
+        {!videoStatus && (
+          <button
+            className="text-indigo-500 font-semibold"
+            onClick={togglePlayback}
+          >
+            Oynat
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
